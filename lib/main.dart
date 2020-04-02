3dbe650a11b1871577cba9d2693e70d2803c3bd0
remote <q.abbasi@micromerger.com>
2020-04-02 19:10:06 +0500

    Btton theme change

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,103 @@
+import 'package:custom_themes/theme_notifier.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main(){
+final darkTheme = ThemeData(
+  buttonColor: Colors.green,
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.black,
+  brightness: Brightness.dark,
+  buttonTheme: ButtonThemeData(
+  textTheme: ButtonTextTheme.accent
+    
+  ),
+ // backgroundColor: const Color(0xFF212121),
+);
 
-      runApp(MyApp() ); //runApp
-}
+final lightTheme = ThemeData(
+  buttonTheme: ButtonThemeData(
+    buttonColor: Colors.redAccent,
+    
+    
+  ),
+  buttonColor: Colors.blue,
+  primarySwatch: Colors.grey,
+  primaryColor: Colors.white,
+  brightness: Brightness.light,
+  backgroundColor: const Color(0xFFE5E5E5),
+  accentColor: Colors.black,
+  accentIconTheme: IconThemeData(color: Colors.white),
+  dividerColor: Colors.white54,
+);
+
+void main() => runApp(
+
+      ChangeNotifierProvider<ThemeNotifier>(
+        create: (_) => ThemeNotifier(darkTheme),
+        child: MyApp(),
+      ),
+    );
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final themeNotifier = Provider.of<ThemeNotifier>(context);
     return MaterialApp(
-             title: 'Button Theme',
-              home:Scaffold(
-                appBar: AppBar(
-                  title: Text('Button Themes'),
-                ),
-                body:Center(
-                          child:Padding(
-                            child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                    RaisedGradientButton(
-                             child: Text('Button 1',
-                             style: TextStyle(color: Colors.white),),
-                             gradient: LinearGradient(
-                             colors: <Color>[Colors.redAccent, Colors.teal],
-                              ),
-                            onPressed: (){
-                            print('button 1 clicked');
-  }
-),
-                  RaisedGradientButton(
-                            child: Text('Button 2',
-                            style: TextStyle(color: Colors.white),),
-                            gradient: LinearGradient(
-                            colors: <Color>[Colors.green, Colors.black],
-  ),
-                           onPressed: (){
-                           print('button 2 clicked');
-  }
-),
-],
- ), padding: EdgeInsets.all(16.0),
-                ),),
-),
- );
-  }
-}
-
-  
-class RaisedGradientButton extends StatelessWidget {
-  final Widget child;
-  final Gradient gradient;
-  final double width;
-  final double height;
-  final Function onPressed;
-
-  const RaisedGradientButton({
-    Key key,
-    @required this.child,
-    this.gradient,
-    this.width = 100.0,
-    this.height = 50.0,
-    this.onPressed,
-  
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: 50.0,
-      margin: EdgeInsets.only(left:20.0),
-      decoration: BoxDecoration(gradient: gradient, boxShadow: [
-        BoxShadow(
-          color: Colors.grey[500],
-          offset: Offset(0.0, 1.5),
-          blurRadius: 1.5,
-        ),
-      ]),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-            onTap: onPressed,
-            child: Center(
-              child: child,
-            )),
-      ),
+      title: 'Button Themes',
+      theme: themeNotifier.getTheme(),
+      home: MyHomePage(title: "Theme",),
     );
   }
 }
+
+class MyHomePage extends StatelessWidget {
+  final String title;
+  const MyHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title),),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Light Theme"),
+              onPressed: (){
+              Provider.of<ThemeNotifier>(context,listen: false).setTheme(lightTheme);
+            },),
+            SizedBox(height: 100),
+
+            Theme(
+              data: ThemeData(buttonColor: Colors.yellow),
+              child:RaisedButton(
+              child: Text("Dark Theme"),
+              onPressed: (){
+              Provider.of<ThemeNotifier>(context,listen: false).setTheme(darkTheme);
+            },) ,)
+          ],
+        ),
+      )
+      ,
+      );
+  }
+}
+
+// class Button extends StatelessWidget {
+//   final ButtonThemeData buttonThemeData;
+//   final String text;
+//   final Function onPressed;
+//   const Button({Key key,@required this.buttonThemeData,@required this.text,@required this.onPressed}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Theme(
+//       data: ThemeData(
+//         buttonTheme: buttonThemeData
+//       ),
+//       child: MaterialButton(child: Text(text),
+//       onPressed: onPressed,),
+//     );
+//   }
+// }
